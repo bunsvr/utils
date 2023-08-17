8f5cec01c6c8926a2eb0f165867fdb4783878369
aquapi <aquaplmc@gmail.com>
2023-08-17 17:11:38 +0700

    Add email and regex

--- src/guard.ts
+++ src/guard.ts
@@ -1,12 +1,15 @@
-export namespace guard {   
+export namespace guard {
     const defaultPropName = 'o',
+        emailFnName = '_e',
+        regexVarPrefix = '_r',
         validator = {
             str: (currentPropName: string) => `typeof ${currentPropName}==='string'`,
             num: (currentPropName: string) => `typeof ${currentPropName}==='number'`,
             bool: (currentPropName: string) => `typeof ${currentPropName}==='boolean'`,
             undef: (currentPropName: string) => `${currentPropName}===undefined`,
             nil: (currentPropName: string) => `${currentPropName}===null`,
-            obj: (currentPropName: string) => `typeof ${currentPropName}==='object'&&${currentPropName}!==null`
+            obj: (currentPropName: string) => `typeof ${currentPropName}==='object'&&${currentPropName}!==null`,
+            email: (currentPropName: string) => `${emailFnName}(${currentPropName})`
         };
 
     // For infer types
@@ -17,12 +20,12 @@ export namespace guard {
     export type OptionalBasicType = keyof {
         [K in BasicType as SuffixQuestionMark<K>]: null
     };
-    
+
     type InferBasic<P> = P extends 'str' ? string : (
         P extends 'num' ? number : (
             P extends 'bool' ? boolean : (
                 P extends 'undef' ? undefined : (
-                   P extends 'nil' ? null : any
+                    P extends 'nil' ? null : any
                 )
             )
         )
@@ -31,14 +34,23 @@ export namespace guard {
     type InferOptionalBasic<T> = T extends `${infer U}?` ? InferBasic<U> | undefined : never;
     export type Infer<T> = T extends BasicType ? InferBasic<T> : (
         T extends OptionalBasicType ? InferOptionalBasic<T> : {
-            [K in keyof T]: Infer<T[K]>       
+            [K in keyof T]: Infer<T[K]>
         }
     );
 
-    export type Validator = Dict<Validator> | BasicType | OptionalBasicType; 
+    export type Validator = Dict<Validator> | BasicType | OptionalBasicType | RegExp;
 
     // Actual code 
     const basicArgs = [], basicValidator = [];
+
+    function checkEmail(email: string) {
+        // Email first part is often longer
+        let i = email.lastIndexOf('@');
+        return i !== -1
+            // Invalid domain if the next char is .
+            && email.charCodeAt(i + 1) !== 46
+            && email.indexOf('.', i + 2) !== -1;
+    }
 
     function getCheckStr(name: string, prop: string) {
         return validator[name](prop);
@@ -47,15 +59,28 @@ export namespace guard {
     function createCheck(
         type: Validator, propName: string, deps: Dict<any>
     ): string {
-        if (typeof type === 'string' && type in validator) 
+        if (typeof type === 'string' && type in validator) {
+            if (type === 'email') deps._hasEmail = true;
+
             return getCheckStr(type, propName);
+        }
+
+        if (type instanceof RegExp) {
+            if (!('_regexIndex' in deps)) deps._regexIndex = 0;
+            const name = regexVarPrefix + deps._regexIndex;
+
+            deps[name] = type;
+            ++deps._regexIndex;
+
+            return `${name}.test(${propName})`;
+        }
 
         if (typeof type === 'object') {
             let vld = validator.obj(propName);
 
-            for (const prop in type) 
+            for (const prop in type)
                 vld += '&&' + createCheck(type[prop], `${propName}.${prop}`, deps);
-            
+
             return vld;
         }
 
@@ -63,10 +88,10 @@ export namespace guard {
             const parentObjEndIndex = propName.lastIndexOf('.'), subProp = propName.substring(parentObjEndIndex + 1);
 
             // This case only returns a literal
-            return `(${parentObjEndIndex === -1 
-                ? validator.undef(propName) 
+            return `(${parentObjEndIndex === -1
+                ? validator.undef(propName)
                 : `!('${subProp}' in ${propName.substring(0, parentObjEndIndex)})`
-            }||${getCheckStr(type.substring(1), propName)})`;
+                }||${getCheckStr(type.substring(1), propName)})`;
         }
 
         // TODO: Compose array 
@@ -77,11 +102,19 @@ export namespace guard {
      * Create a request validator
      */
     export function create<T extends Validator>(type: T, yieldValue: boolean = true): (o?: any) => Infer<T> | null {
-        const deps = {};
+        let deps = {}, body = `return function(${defaultPropName
+            }){return ${createCheck(type, defaultPropName, deps)}${yieldValue ? '?o:null' : ''}}`;
 
-        const body = `return function(${
-            defaultPropName
-        }){return ${createCheck(type, defaultPropName, deps)}${yieldValue ? '?o:null' : ''}}`;
+        if ('_hasEmail' in deps) {
+            let emailCheckFn = checkEmail.toString();
+            emailCheckFn = `function ${emailFnName}${emailCheckFn.substring(emailCheckFn.indexOf('('))
+                };`;
+            body = emailCheckFn + body;
+
+            delete deps._hasEmail;
+        }
+
+        if ('_regexIndex' in deps) delete deps._regexIndex;
 
         return Function(...Object.keys(deps), ...basicArgs, body)(...Object.values(deps), ...basicValidator);
     }
@@ -91,7 +124,7 @@ export namespace guard {
      * Register a custom type
      */
     export function register(
-        type: string, 
+        type: string,
         checkFn: (value: any) => boolean
     ) {
         const fnName = 'v' + registeredTypes;
--- tests/guard.test.ts
+++ tests/guard.test.ts
@@ -42,11 +42,37 @@ test('Nested', () => {
     })).not.toBeNil();
 });
 
+test('Email', () => {
+    const check = guard.create({
+        email: 'email',
+        password: 'str'
+    });
+    console.log(check.toString());
+
+    expect(check({
+        email: 'johndoe_1974@gmail.com',
+        password: '12345678'
+    })).not.toBeNil();
+
+    expect(check({
+        email: 'johndoe_1974@.co',
+        password: '12345678'
+    })).toBeNil();
+});
+
 test('Register types', () => {
     guard.register('fn', (v: any) => typeof v === 'function');
     // @ts-ignore
     const check = guard.create('fn');
     console.log(check.toString())
 
-    expect(check(() => {})).not.toBeNil();
+    expect(check(() => { })).not.toBeNil();
+});
+
+test('RegExp', () => {
+    const check = guard.create(/^a...s$/);
+    console.log(check.toString());
+
+    expect(check('alias')).not.toBeNil();
+    expect(check('a')).toBeNil();
 });
