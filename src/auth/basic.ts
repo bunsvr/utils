5ea4dd7c9f28e5c09ebbdcc4380dfd8fc488d337
aquapi <aquaplmc@gmail.com>
2023-12-15 07:39:38 +0700

    Decode base64

--- src/auth/basic.ts
+++ src/auth/basic.ts
@@ -2,6 +2,9 @@ export type BasicCredentials = [username: string, password: string];
 
 export function extractBasic(header: string): BasicCredentials | null {
     if (header.startsWith('Basic ')) {
+        // Need to decode base64
+        header = atob(header);
+
         var sp = header.indexOf(':', 6);
         if (sp === -1) return null;
 
